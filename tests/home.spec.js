35a58e6948ad70073d4f8cef542e399e42b507f3
Deiximar Virriel <deiximar@gmail.com>
2023-07-09 22:06:47 +0200

    Add test for changing item quantities in the home page

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -43,3 +43,74 @@ test('When the page is in phone mode, the items are displayed one at a time.', a
   expect(await page.screenshot()).toMatchSnapshot('home-page-phone-size.png');
 });
 
+test('Pressing the "plus" (+) button increases the quantity of items one by one.', async ({ page }) => {
+  // GIVEN
+  await page.goto('http://localhost:8080/');
+
+  const item = page.locator('.item').filter({ has: page.getByText('Wrap Dress') });
+  const quantityText = await item.locator(".quantity");
+
+  await expect(quantityText).toHaveText('0');
+
+  // WHEN
+  await item.locator('.bi-plus-lg').click()
+
+  // THEN
+  await expect(quantityText).toHaveText('1');
+});
+
+test('Pressing the "minus" (-) button decreases the quantity of items by one.', async ({ page }) => {
+  //GIVEN
+  await page.goto('http://localhost:8080/');
+
+  const item = page.locator('.item').filter({ has: page.getByText('Wrap Dress') });
+  const quantityText = await item.locator(".quantity");
+  await item.locator('.bi-plus-lg').click()
+
+  await expect(quantityText).toHaveText('1');
+
+  //WHEN
+  await item.locator('.bi-dash-lg').click()
+
+  //THEN
+  await expect(quantityText).toHaveText('0');
+});
+
+test('Pressing the "minus" (-) button when the item quantity is equal to zero, does not decreases the quantity', async ({ page }) => {
+  //GIVEN
+  await page.goto('http://localhost:8080/');
+
+  const item = page.locator('.item').filter({ has: page.getByText('Wrap Dress') });
+  const quantityText = await item.locator(".quantity");
+
+  await expect(quantityText).toHaveText('0');
+
+  //WHEN
+  await item.locator('.bi-dash-lg').click()
+
+  //THEN
+  await expect(quantityText).toHaveText('0');
+});
+
+test('The sum of all item quantities is displayed in the shopping cart.', async ({ page }) => {
+  //GIVEN
+  await page.goto('http://localhost:8080/');
+
+  const cartAmount = page.locator('.cartAmount');
+  const itemOne = page.locator('.item').filter({ has: page.getByText('Wrap Dress') });
+  const itemTwo = page.locator('.item').filter({ has: page.getByText('Ribbed Sweater') });
+
+  const quantityTextOne = await itemOne.locator(".quantity");
+  const quantityTextTwo = await itemTwo.locator(".quantity");
+
+  await expect(quantityTextOne).toHaveText('0');
+  await expect(quantityTextTwo).toHaveText('0');
+
+  //WHEN
+  await itemOne.locator('.bi-plus-lg').click()
+  await itemTwo.locator('.bi-plus-lg').click()
+
+  //THEN
+  await expect(cartAmount).toHaveText('2');
+});
+
